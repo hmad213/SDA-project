db21058cce1ef6f5e6980d829fd9b9377bb19832
hmad213 <142343785+hmad213@users.noreply.github.com>
2026-04-23 18:20:24 +0500

    Add authentication and authorization middleware

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    const message =
+      err.name == "TokenExpiredError" ? "Token Expired" : "Invalid Token";
+    return res.status(401).json({ error: message });
+  }
+};
+
+module.exports = {
+  authenticate,
+};
